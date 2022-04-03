b115eaabc31bd82a7291e516a4f04fb68d1c659a
Carlos11448 <100970045+Carlos11448@users.noreply.github.com>
2022-04-03 16:56:36 -0600

    Update AdultFemale.java

--- edu/ucalgary/ensf409/AdultFemale.java
+++ edu/ucalgary/ensf409/AdultFemale.java
@@ -1,19 +1,33 @@
 /**
-author: Carlos Morera Pinilla
-version: 1.1
+author: Carlos Morera Pinilla and Jaboc Adeyemo
+version: 1.2
 since: 1.0
 */
 package edu.ucalgary.ensf409;
 
-public class AdultFemale
+public class AdultFemale extends ClientDailyNeeds
 {
 	//Private fields:
 	private final int CLIENT_ID = 2;
 	
 	//Constructor:
-	public AdultFemale(String[][] dailyNeeds)
+	public AdultFemale(String[][] dailyNeeds) throws Exception
 	{
-		
+		if (dailyNeeds == null) 
+		{
+			throw new NullPointerException();
+		}
+
+		else {
+			for (int i = 0; i < dailyNeeds.length; i++) 
+			{
+					FoodContent(Integer.parseInt(dailyNeeds[CLIENTID - 1][1]),
+						Integer.parseInt(dailyNeeds[CLIENTID - 1][2]),
+						Integer.parseInt(dailyNeeds[CLIENTID - 1][3]),
+						Integer.parseInt(dailyNeeds[CLIENTID - 1][4]),
+						Integer.parseInt(dailyNeeds[CLIENTID - 1][5]));
+			}
+		}
 	}
 
 }

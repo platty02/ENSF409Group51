ba8c215fde00489c07c14249346ea6f457b4969f
platty02 <101432888+platty02@users.noreply.github.com>
2022-04-03 17:46:58 -0600

    update Item.java to check for invalid input.

--- edu/ucalgary/ensf409/Item.java
+++ edu/ucalgary/ensf409/Item.java
@@ -1,6 +1,7 @@
 //ENSF409 FINAL PROJECT GROUP 51
+//@author JamesPlatt
 //Item.java
-//version: 1.1
+//version: 1.2
 //from: 1.0 
 
 package edu.ucalgary.ensf409;
@@ -20,15 +21,30 @@ public class Item {
     //constructor.
     //Constructor was changed to include itemID and name as variables,
     //this will need to be relflected in the uml. 2022-03-25
-    public Item(String name, String itemID, int grain, int fruitVeg, int protein, int other, int calories){
-        //set private variables.
-        this.itemID = new String(itemID);
-        this.name = new String(name);
-        this.grainContent = grain;
-        this.fruitVegContent = fruitVeg;
-        this.proteinContent = protein;
-        this.other = other;
-        this.calories = calories;
+    public Item(String name, String itemID, int grain, int fruitVeg, int protein, int other, int calories) throws IllegalArgumentException{
+        //checks all input variables for validity. name and itemID should not be null,
+        //all calorie values should be greater than or equal to 0.
+        boolean valid = true;
+        if(name == null|| itemID == null){
+            valid = false;
+        }
+        else if(grain < 0 || fruitVeg < 0 || protein < 0 || other < 0 || calories < 0 ){
+            valid = false;
+        }
+        //if the varialbles are valid, set the private variables.
+        if(valid == true){
+            this.itemID = new String(itemID);
+            this.name = new String(name);
+            this.grainContent = grain;
+            this.fruitVegContent = fruitVeg;
+            this.proteinContent = protein;
+            this.other = other;
+            this.calories = calories;
+        }
+        //else throw exception.
+        else{
+            throw new IllegalArgumentException();
+        }
     }
 
     //grain content getter

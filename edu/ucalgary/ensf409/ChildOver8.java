e736e045300c6412b7dca1fcfd788082e7a55331
Carlos11448 <100970045+Carlos11448@users.noreply.github.com>
2022-04-03 16:50:12 -0600

    Update ChildOver8.java

--- edu/ucalgary/ensf409/ChildOver8.java
+++ edu/ucalgary/ensf409/ChildOver8.java
@@ -9,9 +9,7 @@ public class ChildOver8
 {
 	//Private fields:
 	private final int CLIENT_ID = 4;
-	//NOTE: We shud talk about making a connection to the DataStorage Class to either each client.
-	//Or the ClientDailyNeeds class.
-
+	
 	//Constructor:
 	public ChildOver8(String[][] dailyNeeds)
 	{

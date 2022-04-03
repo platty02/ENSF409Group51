c5418a7aef2c6d7c9b80f016a53b2a821342249c
Carlos11448 <100970045+Carlos11448@users.noreply.github.com>
2022-04-03 16:59:11 -0600

    Update ChildUnder8.java

--- edu/ucalgary/ensf409/ChildUnder8.java
+++ edu/ucalgary/ensf409/ChildUnder8.java
@@ -1,20 +1,31 @@
 /**
-author: Carlos Morera Pinilla
+author: Carlos Morera Pinilla and Jaboc Adeyemo
 version: 1.1
 since: 1.0
 */
 package edu.ucalgary.ensf409;
 
-public class ChildUnder8
+public class ChildUnder8 extends ClientDailyNeeds
 {
 	//Private fields:
 	private final int CLIENT_ID = 3;
-	//NOTE: We shud talk about making a connection to the DataStorage Class to either each client.
-	//Or the ClientDailyNeeds class.
+	
 	//Constructor:
-	public ChildUnder8(String[][] dailyNeeds)
+	public ChildUnder8(String[][] dailyNeeds) throws Exception
 	{
-		
+		if (dailyNeeds == null) {
+			throw new NullPointerException();
+		}
+
+		else {
+			for (int i = 0; i < dailyNeeds.length; i++) {
+				FoodContent(Integer.parseInt(dailyNeeds[CLIENTID - 1][1]),
+						Integer.parseInt(dailyNeeds[CLIENTID - 1][2]),
+						Integer.parseInt(dailyNeeds[CLIENTID - 1][3]),
+						Integer.parseInt(dailyNeeds[CLIENTID - 1][4]),
+						Integer.parseInt(dailyNeeds[CLIENTID - 1][5]));
+			}
+		}
 	}
 
 }

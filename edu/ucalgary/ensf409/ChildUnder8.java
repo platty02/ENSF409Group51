6bbb6d1d020200d884e603cd1f7c0d8549d4c110
Carlos11448 <100970045+Carlos11448@users.noreply.github.com>
2022-04-08 12:35:23 -0600

    Update ChildUnder8.java

--- edu/ucalgary/ensf409/ChildUnder8.java
+++ edu/ucalgary/ensf409/ChildUnder8.java
@@ -13,14 +13,10 @@ public class ChildUnder8 extends ClientDailyNeeds
 	//Constructor:
 	public ChildUnder8(String[][] dailyNeeds) throws Exception
 	{
-		for (int i = 0; i < dailyNeeds.length; i++) 
-		{
-			super(Integer.parseInt(dailyNeeds[CLIENTID - 1][1]),
-				Integer.parseInt(dailyNeeds[CLIENTID - 1][2]),
+		super(Integer.parseInt(dailyNeeds[CLIENTID - 1][2]),
 				Integer.parseInt(dailyNeeds[CLIENTID - 1][3]),
 				Integer.parseInt(dailyNeeds[CLIENTID - 1][4]),
-				Integer.parseInt(dailyNeeds[CLIENTID - 1][5]));
-		}
+				Integer.parseInt(dailyNeeds[CLIENTID - 1][5]);
 	}
 
 }

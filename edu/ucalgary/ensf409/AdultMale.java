57d10b6e92499d1c9acd4aa22269e2836e1de2c0
Carlos11448 <100970045+Carlos11448@users.noreply.github.com>
2022-04-08 12:33:30 -0600

    Update AdultMale.java

--- edu/ucalgary/ensf409/AdultMale.java
+++ edu/ucalgary/ensf409/AdultMale.java
@@ -13,13 +13,11 @@ public class AdultMale extends ClientDailyNeeds
 	//Constructor:
 	public AdultMale(String[][] dailyNeeds) throws Exception
 	{
-		 for (int i = 0; i < dailyNeeds.length; i++) 
-		{
-			super(Integer.parseInt(dailyNeeds[CLIENTID - 1][1]),
+		super(Integer.parseInt(dailyNeeds[CLIENTID - 1][1]),
 				Integer.parseInt(dailyNeeds[CLIENTID - 1][2]),
 				Integer.parseInt(dailyNeeds[CLIENTID - 1][3]),
 				Integer.parseInt(dailyNeeds[CLIENTID - 1][4]),
 				Integer.parseInt(dailyNeeds[CLIENTID - 1][5]));
-		}
+		
 	}
 }

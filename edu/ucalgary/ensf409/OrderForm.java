78349c861affa058c400c81638a28e1b2095ca31
Carlos11448 <100970045+Carlos11448@users.noreply.github.com>
2022-04-08 12:56:29 -0600

    Update OrderForm.java

--- edu/ucalgary/ensf409/OrderForm.java
+++ edu/ucalgary/ensf409/OrderForm.java
@@ -15,14 +15,27 @@ public class OrderForm
 	//Retrives a formatted String of the Order Form taken from the collection of Hampers in HamperList.
 	public String getFormattedForm()
 	{
+		this.br = new BufferedReader(new FileReader(writeOrderForm()));
+		
+	}	
+	//Writes the Order Form to the file
+	public File writeOrderForm(String fileName)
+	{
 		try
 		{
-			this.bw = new BufferedWriter(new FileWriter("orderForm.txt"));
+			this.bw = new BufferedWriter(new FileWriter(fileName + ".txt"));
 			this.bw.write("JJCK Food Bank.......\n");
 			this.bw.write("Hamper Order Form\n\n");
 			this.bw.write("Name: \n");
 			this.bw.write("Date: \n\n");
 			this.bw.write("Original Request: \n");
+			
+			for(int i = 0; i < this.REPORT.length; i++)
+			{
+				this.bw.write(this.REPORT.get(i) + "\n");
+			}
+			
+			return fileName + ".txt";
 			
 		}
 		catch(IOException e)

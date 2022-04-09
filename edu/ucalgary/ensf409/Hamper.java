184a095ccd1f29cb13375a6feeaaeaa155d74c95
KolbyLalonde <100547058+KolbyLalonde@users.noreply.github.com>
2022-04-08 18:42:32 -0600

    Hamper V1.3

--- edu/ucalgary/ensf409/Hamper.java
+++ edu/ucalgary/ensf409/Hamper.java
@@ -2,12 +2,13 @@
 /**
 @author Kolby Lalonde 
 UCID: 30115568
-@version 1.2 April, 8, 2022
+@version 1.3 April, 8, 2022
 @since 1.0 March, 25, 2022
 **/
 
 package edu.ucalgary.ensf409;
 
+import java.util.*;
 // Terminal Command //
 // javac edu/ucalgary/ensf409/Hamper.java
 
@@ -15,6 +16,10 @@ package edu.ucalgary.ensf409;
 /*
 The Hamper class is designed to .......
 */
+
+// javac -cp .:lib/mysql-connector-java-8.0.23.jar edu/ucalgary/ensf409/GUI.java
+
+// java -cp .:lib/mysql-connector-java-8.0.23.jar edu/ucalgary/ensf409/GUI
 
 public class Hamper{
 
@@ -24,15 +29,15 @@ public class Hamper{
     private final int TOTAL_PROTIEN_CALORIES;
     private final int TOTAL_OTHER_CALORIES;
     private ArrayList<String[]> possibleCombos = new ArrayList<String[]>();
-    private ArrayList<Integer> possibleCombosAccess = new ArrayList<Integer>();
+    private ArrayList<Integer> possibleCombosExtra = new ArrayList<Integer>();
 
-    public void Hamper(int[] clientAmount, AdultMale AdultMales, AdultFemale AdultFemales, ChildOver8 ChildO8, ChildUnder8 ChildU8){
+    public Hamper(int[] clientAmount, AdultMale AdultMales, AdultFemale AdultFemales, ChildOver8 ChildO8, ChildUnder8 ChildU8){
 
         // creating variables to hold the intermediate values for each category as each client type is called
         int net = 0;
         int grain = 0;
         int fruitVeggie = 0;
-        int protien = 0;
+        int protein = 0;
         int other = 0;
 
         // Calling each client to get the total net calories across all clients
@@ -45,61 +50,60 @@ public class Hamper{
         this.TOTAL_NET_CALORIES = net;
 
         // Calling each client to get the total grain calories across all clients
-        grain += AdultMales.getCalories() * (AdultMales.getWholeGrains() / 100) * clientAmount[0];
-        grain += AdultFemales.getCalories() * (AdultFemales.getWholeGrains() / 100) * clientAmount[1];
-        grain += ChildO8.getCalories() * (ChildO8.getWholeGrains() / 100) * clientAmount[2];
-        grain += ChildU8.getCalories() * (ChildU8.getWholeGrains() / 100) * clientAmount[3];
+        grain += AdultMales.getCalories() * ((double)AdultMales.getWholeGrains() / 100) * clientAmount[0];
+        grain += AdultFemales.getCalories() * ((double)AdultFemales.getWholeGrains() / 100) * clientAmount[1];
+        grain += ChildO8.getCalories() * ((double)ChildO8.getWholeGrains() / 100) * clientAmount[2];
+        grain += ChildU8.getCalories() * ((double)ChildU8.getWholeGrains() / 100) * clientAmount[3];
 
         // Setting the total grain calories for the hamper
         this.TOTAL_GRAIN_CALORIES = grain;
 
         // Calling each client to get the total fruit veggie calories across all clients
-        fruitVeggie += AdultMales.getCalories() * (AdultMales.getFruitsVeggies() / 100) * clientAmount[0];
-        fruitVeggie += AdultFemales.getCalories() * (AdultFemales.getFruitsVeggies() / 100) * clientAmount[1];
-        fruitVeggie += ChildO8.getCalories() * (ChildO8.getFruitsVeggies() / 100) * clientAmount[2];
-        fruitVeggie += ChildU8.getCalories() * (ChildU8.getFruitsVeggies() / 100) * clientAmount[3];
+        fruitVeggie += AdultMales.getCalories() * ((double)AdultMales.getFruitsVeggies() / 100) * clientAmount[0];
+        fruitVeggie += AdultFemales.getCalories() * ((double)AdultFemales.getFruitsVeggies() / 100) * clientAmount[1];
+        fruitVeggie += ChildO8.getCalories() * ((double)ChildO8.getFruitsVeggies() / 100) * clientAmount[2];
+        fruitVeggie += ChildU8.getCalories() * ((double)ChildU8.getFruitsVeggies() / 100) * clientAmount[3];
 
         // Setting the total fruit veggie calories for the hamper
         this.TOTAL_FRUITVEGGIE_CALORIES = fruitVeggie;
 
         // Calling each client to get the total protein calories across all clients
-        protein += AdultMales.getCalories() * (AdultMales.getProtein() / 100) * clientAmount[0];
-        protein += AdultFemales.getCalories() * (AdultFemales.getProtein() / 100) * clientAmount[1];
-        protein += (ChildO8.getProtein() / 100) * clientAmount[2];
-        protein += (ChildU8.getProtein() / 100) * clientAmount[3];
+        protein += AdultMales.getCalories() * ((double)AdultMales.getProtein() / 100) * clientAmount[0];
+        protein += AdultFemales.getCalories() * ((double)AdultFemales.getProtein() / 100) * clientAmount[1];
+        protein += ChildO8.getCalories() * ((double)ChildO8.getProtein() / 100) * clientAmount[2];
+        protein += ChildU8.getCalories() * ((double)ChildU8.getProtein() / 100) * clientAmount[3];
 
         // Setting the total protein calories for the hamper
         this.TOTAL_PROTIEN_CALORIES = protein;
 
         // Calling each client to get the total other calories across all clients
-        other += AdultMales.getCalories() * (AdultMales.getOther() / 100) * clientAmount[0];
-        other += AdultFemales.getCalories() * (AdultFemales.getOther() / 100) * clientAmount[1];
-        other += ChildO8.getCalories() * (ChildO8.getOther() / 100) * clientAmount[2];
-        other += ChildU8.getCalories() * (ChildU8.getOther() / 100) * clientAmount[3];
+        other += AdultMales.getCalories() * ((double)AdultMales.getOther() / 100) * clientAmount[0];
+        other += AdultFemales.getCalories() * ((double)AdultFemales.getOther() / 100) * clientAmount[1];
+        other += ChildO8.getCalories() * ((double)ChildO8.getOther() / 100) * clientAmount[2];
+        other += ChildU8.getCalories() * ((double)ChildU8.getOther() / 100) * clientAmount[3];
 
         // Setting the total other calories for the hamper
         this.TOTAL_OTHER_CALORIES = other;
     }
 
-    public String[] calculateOptimalHamper(AvaliableFood avaliableFoods){
+    public String[] calculateOptimalHamper(AvailibleFood avaliableFoods){
 
         ArrayList <Item> foodOptions = avaliableFoods.returnList();
 
         int amount = foodOptions.size();
 
-        Item[] data = new Item[amount];
+        String[] data = new String[amount];
 
         for (int num = 0; num <= amount; num++){  
             combinations(avaliableFoods, data, 0, amount - 1, 0, num);
         }
 
         String[] myOptimalHamper = returnOptimalHamper();
-
-        return myOptimalHAmper;
-
+        
+        return myOptimalHamper;
     }
 
-    public void combinations(AvaliableFood avaliableFoods, Item[] data, int start, int end, int index, int num) {
+    public void combinations(AvailibleFood avaliableFoods, String[] data, int start, int end, int index, int num) {
 
         ArrayList <Item> inventory = avaliableFoods.returnList();
 
@@ -112,7 +116,7 @@ public class Hamper{
             int totalCalories = 0;
             for(int j = 0; j < num; j++){
                 int x = 0;
-                while(inventory.get(i).getID() != data[x]){
+                while(!inventory.get(x).getID().equals(data[j])){
                     x++;
                 }
                 totalGrain += inventory.get(x).getGrainContent();
@@ -120,8 +124,9 @@ public class Hamper{
                 totalProtein += inventory.get(x).getProteinContent();
                 totalOther += inventory.get(x).getOtherContent();
                 totalCalories += inventory.get(x).getCalories();
+
             }
-            checkValid(data, totalGrain, totalFV, totalProtein, totalOther, totalCalories);
+            checkValid(data, num, totalGrain, totalFV, totalProtein, totalOther, totalCalories);
         }
  
         for (int i = start; i <= end && ((end - i + 1) >= (num - index)); i++) {
@@ -130,22 +135,23 @@ public class Hamper{
         }   
     }
 
-    public void checkValid(String[] data, int grain, int fruitVeggies, int protien, int other, int calories){
+    public void checkValid(String[] data, int num, int grain, int fruitVeggies, int protein, int other, int calories){
 
-        if(grain >= this.TOTAL_GRAIN_CALORIES && grain < (2 * this.TOTAL_GRAIN_CALORIES)){
-            if(fruitVeggies >= this.TOTAL_FRUITVEGGIE_CALORIES && fruitVeggies < (2 * this.TOTAL_FRUITVEGGIE_CALORIES)){
-                if(protien >= this.TOTAL_PROTIEN_CALORIES && protien < (2 * this.TOTAL_PROTIEN_CALORIES)){
-                    if(other >= this.TOTAL_OTHER_CALORIES && other < (2 * this.TOTAL_OTHER_CALORIES)){
-                        if(calories >= this.TOTAL_NET_CALORIES && calories < (2 * this.TOTAL_NET_CALORIES)){
-                            int access = (grain - this.TOTAL_GRAIN_CALORIES );
-                            access += (fruitVeggies - this.TOTAL_FRUITVEGGIE_CALORIES);
-                            access += (protien - this.TOTAL_PROTIEN_CALORIES );
-                            access += (other - this.TOTAL_OTHER_CALORIES);
-                            access += (calories - this.TOTAL_NET_CALORIES);
-                            this.possibleCombos.add(data);
-                            this.possibleCombosAccess.add(access);
-                        }
+        if(grain >= this.TOTAL_GRAIN_CALORIES && fruitVeggies >= this.TOTAL_FRUITVEGGIE_CALORIES){
+            if(protein>= this.TOTAL_PROTIEN_CALORIES && other >= this.TOTAL_OTHER_CALORIES){
+                if(calories >= this.TOTAL_NET_CALORIES){
+                    String [] combo = new String[num];
+                    for (int j = 0; j < num; j++){
+                        combo[j] = data[j];
                     }
+                    int extra = (grain - this.TOTAL_GRAIN_CALORIES );
+                    extra += (fruitVeggies - this.TOTAL_FRUITVEGGIE_CALORIES);
+                    extra += (protein - this.TOTAL_PROTIEN_CALORIES );
+                    extra += (other - this.TOTAL_OTHER_CALORIES);
+                    extra += (calories - this.TOTAL_NET_CALORIES);
+                    this.possibleCombos.add(combo);
+                    this.possibleCombosExtra.add(extra);
+        
                 }
             }
         }
@@ -153,15 +159,17 @@ public class Hamper{
 
     public String[] returnOptimalHamper(){
 
-        if(this.possibleCombosAccess.size() == 0){
-            // throw exception here! no hamers found !!!!!!!!!!!!
+        if(this.possibleCombosExtra.size() == 0){
+            // through exception here! no hamers found !!!!!!!!!!!!
+            String[] empty = new String[1];
+            return empty;
         }
         
-        int minAccess = this.possibleCombosAccess.get(0);
+        int minAccess = this.possibleCombosExtra.get(0);
         int index = 0;
-        for(int i = 0; i < this.possibleCombosAccess.size(); i++){
-            if(minAccess > this.possibleCombosAccess.get(i)){
-                minAccess = this.possibleCombosAccess.get(i);
+        for(int i = 0; i < this.possibleCombosExtra.size(); i++){
+            if(minAccess > this.possibleCombosExtra.get(i)){
+                minAccess = this.possibleCombosExtra.get(i);
                 index = i;
             }
         }

afb1cdc7ae6eee4ff43a94515a08716753bc3d17
Carlos11448 <100970045+Carlos11448@users.noreply.github.com>
2022-04-09 13:05:30 -0600

    Update OrderForm.java

--- edu/ucalgary/ensf409/OrderForm.java
+++ edu/ucalgary/ensf409/OrderForm.java
@@ -14,7 +14,7 @@ public class OrderForm
 	private BufferedReader br;
 	private BufferedWriter bw;
 	
-	//Constructor:
+	 //Constructor:
 	public OrderForm(HamperList reportInfo)
 	{
 		this.REPORT = reportInfo;
@@ -39,20 +39,47 @@ public class OrderForm
 			System.err.println("orderForm.txt could not be read, it may not exist yet!");
 			System.exit(1);
 		}
+		//Attempt to close the input file.
+		finally
+		{
+			try
+			{
+				this.br.close();
+			}
+			catch(IOException e)
+			{
+				System.err.println("orderForm.txt after being read could not be closed!");
+				System.exit(1);
+			}
+		}
 	}	
 	//Writes the Order Form to the file
 	public File writeOrderForm()
 	{
 		try
 		{
+			int k = 0;
 			this.bw = new BufferedWriter(new FileWriter("orderForm.txt"));
 			this.bw.write("JJCK Food Bank.......\n");
 			this.bw.write("Hamper Order Form\n\n");
 			this.bw.write("Name: \n");
 			this.bw.write("Date: \n\n");
 			this.bw.write("Original Request: \n");
+			for(int i = 0; i < this.REPORT.getHamperCount(); i++)
+			{
+				this.bw.write("Hamper " + (i + 1) + ": ");
+				while(k < 4)
+				{
+					if(Hamper.getNumOfClientsAt(k) != 0)
+					{
+						this.bw.write(Hamper.getNumOfClientsAt(k) + ", ");
+					}
+					k++;
+				}
+				this.bw.write("\n");
+			}
 			
-			for(int i = 0; i < this.REPORT.length; i++)
+			for(int i = 0; i < this.REPORT.getHamperCount(); i++)
 			{
 				this.bw.write("Hamper " + (i + 1) + " Items:\n");
 				this.bw.write(this.REPORT.get(i) + "\n");
@@ -62,8 +89,21 @@ public class OrderForm
 		}
 		catch(IOException e)
 		{
-			System.err.println("Writing to the OrderForm file failed in getFormattedForm()!");
+			System.err.println("orderForm.txt could not be written to!");
 			System.exit(1);
+		}
+		//Attempt to close the output file.
+		finally
+		{
+			try
+			{
+				this.bw.close();
+			}
+			catch(IOException e)
+			{
+				System.err.println("orderForm.txt could not be closed!");
+				System.exit(1);
+			}
 		}
 	}
 }

b6b5e6fcd591eef50bab2a1b1874b89cf49db1c9
platty02 <101432888+platty02@users.noreply.github.com>
2022-04-09 13:19:16 -0600

    Update HamperList to include calculateOrder function

--- edu/ucalgary/ensf409/HamperList.java
+++ edu/ucalgary/ensf409/HamperList.java
@@ -40,4 +40,28 @@ public class HamperList
 	{
 		this.hamperArray.clear();
 	}
+	//public Class to calculate the optimal setup of food items.
+	public void calculateOrder(AvailibleFood availibleFood){
+		for(int i =0; i < this.hamperArray.size(); i++){
+			//get the optimal hamper in string format
+			String[] foodInStrings = hamperArray.get(i).calculateOptimalHamper(availibleFood);
+			//if a optimal hamper exists, store it as a ItemArray in the Hamper.
+			if(foodInStrings != null){	
+				Item[] foodInItems = new Item[foodInStrings.length];
+			
+				for(int j =0; j < foodInItems.length; j++){
+					foodInItems[j] = availibleFood.remove(foodInStrings[j]);
+				}
+				hamperArray.get(i).setItems(foodInItems);
+			}
+			//else no optimal hamper can be made, so insuffcient resources.
+			else{
+				//throw new UnavailibleResourcesException.
+			}
+		}
+		//now write to file using OrderForm.
+		//eneter writting to file code here
+
+	}
+
 }

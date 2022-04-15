b6f48b282bd2199cf1280cba8be846c042be6e62
platty02 <101432888+platty02@users.noreply.github.com>
2022-04-15 10:15:27 -0600

    Update OrderForm.java 1.5
    Add comments and clean up structure of the file

--- edu/ucalgary/ensf409/OrderForm.java
+++ edu/ucalgary/ensf409/OrderForm.java
@@ -1,6 +1,6 @@
 /**
 author: Carlos Morera Pinilla and Jacob Adeyemo and James Platt
-version: 1.4
+version: 1.5
 since: 1.0
 */
 
@@ -17,167 +17,63 @@ public class OrderForm
 	//Private fields:
 	private final ArrayList<Hamper> REPORT;
 	
-	//Constructor:
+	//Constructor: Fills the REPORT variable with list of Hampers.
 	public OrderForm(ArrayList<Hamper> reportInfo)
 	{
 		this.REPORT = reportInfo;
 	}
-	//Retrives a formatted String of the Order Form taken from the collection of Hampers in HamperList.
+	//Creates a formatted order form using the order information stored in each hamper in REPORT. 
+	//Then, writes this information to a text file namws Order.txt
 	public void writeOrderForm(){
         try{
+			//Create a text file using name Order and write the title Info
             FileWriter writeOrder = new FileWriter(new File("Order.txt"));
             writeOrder.write("Example Food Banks\nFood Order Form\n\nName:\nDate:\n\nOriginal Request\n");
+			//For each Hamper in the list, write it's number in the list followed by the
+			//number of client types.
             for(int i =0; i < REPORT.size(); i++){
-                writeOrder.write("Hamper " + i + ": ");
+				//hamper number
+                writeOrder.write("Hamper " + (i+1) + ":");
                 String toWrite = new String();
+				//create String of Clients
                 if(REPORT.get(i).getNumOfClientsAt(0) > 0){
-                    toWrite += (REPORT.get(i).getNumOfClientsAt(0) + " Adult Male," );
+                    toWrite += (" " + REPORT.get(i).getNumOfClientsAt(0) + " Adult Male," );
                 }
                 if(REPORT.get(i).getNumOfClientsAt(1) > 0){
-                    toWrite += (REPORT.get(i).getNumOfClientsAt(1) + " Adult Female," );
+                    toWrite += (" " + REPORT.get(i).getNumOfClientsAt(1) + " Adult Female," );
                 }
                 if(REPORT.get(i).getNumOfClientsAt(2) > 0){
-                    toWrite += (REPORT.get(i).getNumOfClientsAt(2) + " Child Over 8," );
+                    toWrite += (" " + REPORT.get(i).getNumOfClientsAt(2) + " Child Over 8," );
                 }
                 if(REPORT.get(i).getNumOfClientsAt(3) > 0){
-                    toWrite += (REPORT.get(i).getNumOfClientsAt(3) + " Child Under 8," );
+                    toWrite += (" " + REPORT.get(i).getNumOfClientsAt(3) + " Child Under 8," );
                 }
+				//write the Clients minus the extra comma
                 toWrite = toWrite.substring(0, toWrite.length() -1);
                 writeOrder.write(toWrite + "\n");   
             }
             writeOrder.write("\n");
+			//now go through all hampers and write down their orders Item names and ItemID
             for(int i =0; i < REPORT.size(); i++){
-                writeOrder.write("Hamper " + i + " Items:\n");
+				//hamper number
+                writeOrder.write("Hamper " + (i + 1) + " Items:\n");
+				//write hamper items.
                 for(int k =0; k < REPORT.get(i).getItems().length; k++){
                     writeOrder.write(String.format("%-3s\t\t%s", REPORT.get(i).getItems()[k].getID(),REPORT.get(i).getItems()[k].getName()));
                     if(k != REPORT.get(i).getItems().length -1)
                         writeOrder.write("\n");
                 }
+				//if not the last hamper, add two line spacing.
                 if(i != REPORT.size() -1){
                     writeOrder.write("\n");
                     writeOrder.write("\n");
                 }
             }
-            try
-	    {
-           	 writeOrder.close();
-	    }
-            catch(IOException e)
-	    {
-	    	System.err.println("Could not close the output file in writeOrder()!");
-		System.exit(1);
-	    }
+            
+            writeOrder.close();
         }
         catch(IOException e){
-            System.err.println("Could not write to the output file in writeOrder()!");
-	    System.exit(1);
+            //do some sort of error responese here?
         }
     }
 }
-/**
-@author: Carlos Morera Pinilla and Jacob Adeyemo
-@version: 1.4
-@since: 1.0
-*/
-/*
-package edu.ucalgary.ensf409;
-import java.io.*;
-public class OrderForm
-{
-	//Private fields:
-	private final HamperList REPORT;
-	private BufferedReader br;
-	private BufferedWriter bw;
-	
-	 //Constructor:
-	public OrderForm(HamperList reportInfo)
-	{
-		this.REPORT = reportInfo;
-	}
-	//Retrives a formatted String of the Order Form taken from the collection of Hampers in HamperList.
-	public String getFormattedForm()
-	{
-		String formattedString = new String();
-		try
-		{
-			this.br = new BufferedReader(new FileReader("orderForm.txt"));
-			String s = new String();
-			while((s = br.readLine()) != null)
-			{
-				//Read each line
-				formattedString += s;
-			}
-		}
-		catch(IOException e)
-		{
-			System.err.println("orderForm.txt could not be read, it may not exist yet!");
-			System.exit(1);
-		}
-		//Attempt to close the input file and return the String.
-		finally
-		{
-			try
-			{
-				this.br.close();
-			}
-			catch(IOException e)
-			{
-				System.err.println("orderForm.txt after being read could not be closed!");
-				System.exit(1);
-			}
-		}
-		return formattedString;
-	}	
-	//Writes the Order Form to the file
-	public void writeOrderForm()
-	{
-		try
-		{
-			int k = 0;
-			this.bw = new BufferedWriter(new FileWriter("orderForm.txt"));
-			this.bw.write("JJCK Food Bank.......\n");
-			this.bw.write("Hamper Order Form\n\n");
-			this.bw.write("Name: \n");
-			this.bw.write("Date: \n\n");
-			this.bw.write("Original Request: \n");
-			//Writes to the file, all client types involved in each Hamper.
-			for(int i = 0; i < this.REPORT.getHamperCount(); i++)
-			{
-				this.bw.write("Hamper " + (i + 1) + ": ");
-				while(k < 4)
-				{
-					if(this.REPORT.getHamperArray().get(i).getNumOfClientsAt(k) != 0)
-					{
-						this.bw.write(this.REPORT.getHamperArray().get(i).getNumOfClientsAt(k) + ", ");
-					}
-					k++;
-				}
-				this.bw.write("\n");
-			}
-			//Writes all items in the Hamper.
-			for(int i = 0; i < this.REPORT.getHamperCount(); i++)
-			{
-				this.bw.write("Hamper " + (i + 1) + " Items:\n");
-				this.bw.write(this.REPORT.get(i) + "\n");
-			}
-		}
-		catch(IOException e)
-		{
-			System.err.println("orderForm.txt could not be written to!");
-			System.exit(1);
-		}
-		//Attempt to close the output file.
-		finally
-		{
-			try
-			{
-				this.bw.close();
-			}
-			catch(IOException e)
-			{
-				System.err.println("orderForm.txt could not be closed!");
-				System.exit(1);
-			}
-		}
-	}
-}*/

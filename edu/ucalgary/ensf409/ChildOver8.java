bc42a66582c36d7bbb66642b9f674c2bf632f410
platty02 <101432888+platty02@users.noreply.github.com>
2022-04-15 10:41:46 -0600

    Update ChildOver8.java 1.3.1

--- edu/ucalgary/ensf409/ChildOver8.java
+++ edu/ucalgary/ensf409/ChildOver8.java
@@ -1,6 +1,6 @@
 /**
-author Kolby Lalonde James Platt Carlos Morrea Jacob Adeyemo
-version: 1.3
+author Kolby Lalonde, James Platt, Carlos Morera Pinilla, Jacob Adeyemo
+version: 1.3.1
 since: 1.0
 */
 package edu.ucalgary.ensf409;
